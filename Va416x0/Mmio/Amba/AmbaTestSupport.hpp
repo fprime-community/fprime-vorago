1845f8ef4cd5aa03ce33989a1f2fd10ac9573aeb
bevinduckett <bevin.k.duckett@jpl.nasa.gov>
2026-08-27 08:54:52 -0700

    Update copyright year in AmbaTestSupport.hpp

--- Va416x0/Mmio/Amba/AmbaTestSupport.hpp
+++ Va416x0/Mmio/Amba/AmbaTestSupport.hpp
@@ -1,4 +1,4 @@
-// Copyright 2025 California Institute of Technology
+// Copyright 2026 California Institute of Technology
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
